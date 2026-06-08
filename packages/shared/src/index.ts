53253add2fbb46abd8ebf9f5dba5268d5a44eae3
LuminaQAQ <53186053+LuminaQAQ@users.noreply.github.com>
2026-06-09 02:20:36 +0800

    chore(shared): 搭建共享包 TypeScript 项目结构
    创建 tsconfig.json 与 src/{types,constants,validators,utils} 目录结构

---
+++ packages/shared/src/index.ts
@@ -0,0 +1,15 @@
+export * from './types/schema'
+export * from './types/meta'
+export * from './types/event'
+export * from './types/style'
+export * from './types/project'
+export * from './types/api'
+
+export * from './constants'
+
+export * from './validators/schemaValidator'
+export * from './validators/componentValidator'
+
+export * from './utils/idGenerator'
+export * from './utils/treeUtils'
+export * from './utils/cloneUtils'
